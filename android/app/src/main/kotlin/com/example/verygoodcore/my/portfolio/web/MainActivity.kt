c72300a653b867a6228de44eb48d0af7fe218bc2
SakshamMustFin <s.saksham@mufin.co.kr>
2025-04-18 00:46:44 +0530

    Add Windows runner files for Flutter application
    - Created runner executable manifest to define application settings and DPI awareness.
    - Implemented utility functions for console creation and command line argument handling in utils.cpp and utils.h.
    - Developed Win32Window class for managing high DPI-aware windows, including message handling and child content management in win32_window.cpp and win32_window.h.

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/my/portfolio/web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.my_portfolio_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:my_portfolio_web/app/app.dart';
+import 'package:my_portfolio_web/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
