e1cf8170003719b6bd7e74219caff8d1f3244e7e
SakshamMustFin <s.saksham@mufin.co.kr>
2025-04-21 15:18:20 +0530

    refactor: Simplify SVG mocking and remove unused methods in svg_mock.dart

--- test/helpers/google_fonts_test_helper.dart
+++ test/helpers/google_fonts_test_helper.dart
@@ -1,64 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-import '../mocks/google_fonts_mock.dart';
+/// Mock implementation of Google Fonts for testing
+TextStyle mockMontserrat({
+  TextStyle? textStyle,
+  Color? color,
+  FontWeight? fontWeight,
+  double? fontSize,
+  double? letterSpacing,
+  double? height,
+}) {
+  return TextStyle(
+    fontFamily: 'Montserrat',
+    color: color,
+    fontWeight: fontWeight,
+    fontSize: fontSize,
+    letterSpacing: letterSpacing,
+    height: height,
+  );
+}
 
-/// A helper class to patch GoogleFonts methods for testing
-class GoogleFontsTestHelper {
-  /// Patches the GoogleFonts.montserrat method for testing
-  static void patchGoogleFontsMontserrat() {
-    // Create a test binding if not already created
-    TestWidgetsFlutterBinding.ensureInitialized();
-
-    // Mock the GoogleFonts configuration
-    mockGoogleFonts();
-  }
-
-  /// Creates a mock TextTheme with Montserrat font
-  static TextTheme createMockMontserratTextTheme(TextTheme baseTheme) {
-    return TextTheme(
-      displayLarge: mockMontserrat(
-        fontSize: 32,
-        fontWeight: FontWeight.bold,
-        color: baseTheme.displayLarge?.color,
-      ),
-      displayMedium: mockMontserrat(
-        fontSize: 28,
-        fontWeight: FontWeight.bold,
-        color: baseTheme.displayMedium?.color,
-      ),
-      displaySmall: mockMontserrat(
-        fontSize: 24,
-        fontWeight: FontWeight.bold,
-        color: baseTheme.displaySmall?.color,
-      ),
-      headlineMedium: mockMontserrat(
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-        color: baseTheme.headlineMedium?.color,
-      ),
-      headlineSmall: mockMontserrat(
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
-        color: baseTheme.headlineSmall?.color,
-      ),
-      titleLarge: mockMontserrat(
-        fontSize: 16,
-        fontWeight: FontWeight.bold,
-        color: baseTheme.titleLarge?.color,
-      ),
-      bodyLarge: mockMontserrat(
-        fontSize: 16,
-        color: baseTheme.bodyLarge?.color,
-      ),
-      bodyMedium: mockMontserrat(
-        fontSize: 14,
-        color: baseTheme.bodyMedium?.color,
-      ),
-      bodySmall: mockMontserrat(
-        fontSize: 12,
-        color: baseTheme.bodySmall?.color,
-      ),
-    );
-  }
+/// Setup Google Fonts for testing
+void setupGoogleFontsMock() {
+  GoogleFonts.config.allowRuntimeFetching = false;
 }
--- test/mocks/svg_mock.dart
+++ test/mocks/svg_mock.dart
@@ -1,60 +1,42 @@
-import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-/// Sets up mocks for SVG loading in tests
-void mockSvgLoading() {
-  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-      .setMockMethodCallHandler(
-    const MethodChannel('flutter/assets'),
-    (MethodCall methodCall) async {
-      if (methodCall.method == 'loadString') {
-        final path = methodCall.arguments as String;
+/// Mock implementation for SVG functionality in tests
+class SvgMock {
+  /// Create a mock SVG picture for testing
+  static SvgPicture createMockSvg({
+    required String assetName,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    Color? color,
+  }) {
+    return SvgPicture.string(
+      '''
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z"/>
+      </svg>
+      ''',
+      width: width,
+      height: height,
+      fit: fit,
+      colorFilter:
+          color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null,
+    );
+  }
 
-        // Return a simple SVG string for any SVG path
-        if (path.endsWith('.svg')) {
-          return _getMockSvg(path);
-        }
-      }
+  /// Setup SVG mocking for the test environment
+  static void setupSvgMocking() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMessageHandler('plugins.flutter.io/svg', (message) async {
       return null;
-    },
-  );
-}
+    });
+  }
 
-/// Cleans up SVG mocks after tests
-void cleanupSvgMock() {
-  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-      .setMockMethodCallHandler(
-    const MethodChannel('flutter/assets'),
-    null,
-  );
-}
-
-/// Returns a mock SVG string based on the path
-String _getMockSvg(String path) {
-  // Extract the file name from the path
-  final fileName = path.split('/').last.split('.').first;
-
-  // Return different SVGs based on the file name
-  switch (fileName) {
-    case 'flutter':
-      return '''
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-          <path d="M12 2L2 12h4l6-6 6 6h4L12 2z"/>
-        </svg>
-      ''';
-    case 'dart':
-      return '''
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-          <path d="M4 4h16v16H4z"/>
-        </svg>
-      ''';
-    case 'nonexistent':
-      throw Exception('File not found');
-    default:
-      return '''
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-          <circle cx="12" cy="12" r="10"/>
-        </svg>
-      ''';
+  /// Cleanup SVG mocking after tests
+  static void cleanupSvgMocking() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMessageHandler('plugins.flutter.io/svg', null);
   }
 }
