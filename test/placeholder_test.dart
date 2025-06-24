e1f21d24743b4efd780236ba4b3ed29c9a5ea560
SakshamMustFin <s.saksham@mufin.co.kr>
2025-06-25 03:31:49 +0530

    style: fix quote style in placeholder test
    - Changed double quotes to single quotes to match project style
    - Ensures linting passes for pre-push checks

--- test/placeholder_test.dart
+++ test/placeholder_test.dart
@@ -1,7 +1,7 @@
-import "package:flutter_test/flutter_test.dart";
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test("placeholder test", () {
+  test('placeholder test', () {
     expect(true, isTrue);
   });
 }
